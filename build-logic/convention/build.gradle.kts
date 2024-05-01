0e5f7c6cb1dd14dbc9522574db275982ccc0a84f
citytexi <heekyne66@gmail.com>
2024-05-01 15:30:30 +0900

    컨벤션 추가

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -17,43 +17,43 @@ dependencies {
 }
 
 gradlePlugin {
-//    plugins {
-//        create("android-application") {
-//            id = "com.captures.soongan.application"
-//            implementationClass = "com.captures.soongan.plugin.AndroidApplicationPlugin"
-//        }
-//        create("android-library") {
-//            id = "com.captures.soongan.library"
-//            implementationClass = "com.captures.soongan.plugin.AndroidLibraryPlugin"
-//        }
-//        create("android-kotlin") {
-//            id = "com.captures.soongan.kotlin"
-//            implementationClass = "com.captures.soongan.plugin.AndroidKotlinPlugin"
-//        }
-//        create("android-hilt") {
-//            id = "com.captures.soongan.hilt"
-//            implementationClass = "com.captures.soongan.plugin.AndroidHiltPlugin"
-//        }
-//        create("android-room") {
-//            id = "com.captures.soongan.room"
-//            implementationClass = "com.captures.soongan.plugin.AndroidRoomConventionPlugin"
-//        }
-//        create("kotlin-serialization") {
-//            id = "com.captures.soongan.serialization"
-//            implementationClass = "com.captures.soongan.plugin.KotlinSerializationPlugin"
-//        }
-//        create("junit5") {
-//            id = "ccom.captures.soongan.junit5"
-//            implementationClass = "com.captures.soongan.plugin.JUnit5Plugin"
-//        }
-//        create("android-test") {
-//            id = "com.captures.soongan.test"
-//            implementationClass = "com.captures.soongan.plugin.AndroidTestPlugin"
-//        }
-//        create("compose") {
-//            id = "com.captures.soongan.compose"
-//            implementationClass = "com.captures.soongan.plugin.ComposePlugin"
-//        }
-//    }
+    plugins {
+        create("android-application") {
+            id = "com.captures.soongan.application"
+            implementationClass = "com.captures.soongan.plugin.AndroidApplicationPlugin"
+        }
+        create("android-library") {
+            id = "com.captures.soongan.library"
+            implementationClass = "com.captures.soongan.plugin.AndroidLibraryPlugin"
+        }
+        create("android-kotlin") {
+            id = "com.captures.soongan.kotlin"
+            implementationClass = "com.captures.soongan.plugin.AndroidKotlinPlugin"
+        }
+        create("android-hilt") {
+            id = "com.captures.soongan.hilt"
+            implementationClass = "com.captures.soongan.plugin.AndroidHiltPlugin"
+        }
+        create("android-room") {
+            id = "com.captures.soongan.room"
+            implementationClass = "com.captures.soongan.plugin.AndroidRoomConventionPlugin"
+        }
+        create("kotlin-serialization") {
+            id = "com.captures.soongan.serialization"
+            implementationClass = "com.captures.soongan.plugin.KotlinSerializationPlugin"
+        }
+        create("junit5") {
+            id = "ccom.captures.soongan.junit5"
+            implementationClass = "com.captures.soongan.plugin.JUnit5Plugin"
+        }
+        create("android-test") {
+            id = "com.captures.soongan.test"
+            implementationClass = "com.captures.soongan.plugin.AndroidTestPlugin"
+        }
+        create("compose") {
+            id = "com.captures.soongan.compose"
+            implementationClass = "com.captures.soongan.plugin.ComposePlugin"
+        }
+    }
 }
 
