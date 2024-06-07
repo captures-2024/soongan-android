ffadfbe51d2b48bbb138e930500400bfbd2afb74
citytexi <heekyne66@gmail.com>
2024-06-07 14:28:56 +0900

    BottomBar 컴포넌트 추가

---
+++ core/designSystem/src/main/kotlin/com/captures2024/soongan/core/designsystem/icon/myiconpack/IconFeed.kt
@@ -0,0 +1,53 @@
+package com.captures2024.soongan.core.designsystem.icon.myiconpack
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathFillType.Companion.NonZero
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.StrokeCap.Companion.Butt
+import androidx.compose.ui.graphics.StrokeJoin.Companion.Miter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.ImageVector.Builder
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.unit.dp
+import com.captures2024.soongan.core.designsystem.icon.MyIconPack
+
+public val MyIconPack.IconFeed: ImageVector
+    get() {
+        if (_iconFeed != null) {
+            return _iconFeed!!
+        }
+        _iconFeed = Builder(name = "IconFeed", defaultWidth = 24.0.dp, defaultHeight = 24.0.dp,
+                viewportWidth = 24.0f, viewportHeight = 24.0f).apply {
+            path(fill = SolidColor(Color(0xFFD6D5D2)), stroke = null, strokeLineWidth = 0.0f,
+                    strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
+                    pathFillType = NonZero) {
+                moveTo(21.3333f, 2.6667f)
+                verticalLineTo(21.3333f)
+                horizontalLineTo(2.6667f)
+                verticalLineTo(2.6667f)
+                horizontalLineTo(21.3333f)
+                close()
+                moveTo(21.3333f, 0.0f)
+                horizontalLineTo(2.6667f)
+                curveTo(1.2f, 0.0f, 0.0f, 1.2f, 0.0f, 2.6667f)
+                verticalLineTo(21.3333f)
+                curveTo(0.0f, 22.8f, 1.2f, 24.0f, 2.6667f, 24.0f)
+                horizontalLineTo(21.3333f)
+                curveTo(22.8f, 24.0f, 24.0f, 22.8f, 24.0f, 21.3333f)
+                verticalLineTo(2.6667f)
+                curveTo(24.0f, 1.2f, 22.8f, 0.0f, 21.3333f, 0.0f)
+                close()
+                moveTo(14.8533f, 11.8133f)
+                lineTo(10.8533f, 16.9733f)
+                lineTo(8.0f, 13.52f)
+                lineTo(4.0f, 18.6667f)
+                horizontalLineTo(20.0f)
+                lineTo(14.8533f, 11.8133f)
+                close()
+            }
+        }
+        .build()
+        return _iconFeed!!
+    }
+
+private var _iconFeed: ImageVector? = null
---
+++ feature/main/src/main/kotlin/com/captures2024/soongan/feature/main/ui/SoonGanBottomBar.kt
@@ -0,0 +1,49 @@
+package com.captures2024.soongan.feature.main.ui
+
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hierarchy
+import com.captures2024.soongan.core.designsystem.component.SoonGanNavigationBar
+import com.captures2024.soongan.core.designsystem.component.SoonGanNavigationBarItem
+import com.captures2024.soongan.feature.main.navigation.TopLevelDestination
+
+@Composable
+internal fun SoonGanBottomBar(
+    destinations: List<TopLevelDestination>,
+    onNavigateToDestination: (TopLevelDestination) -> Unit,
+    currentDestination: NavDestination?,
+    modifier: Modifier = Modifier,
+) {
+    SoonGanNavigationBar(
+        modifier = modifier,
+    ) {
+        destinations.forEach { destination ->
+            val selected = currentDestination.isTopLevelDestinationInHierarchy(destination)
+            SoonGanNavigationBarItem(
+                selected = selected,
+                onClick = { onNavigateToDestination(destination) },
+                icon = {
+                    Icon(
+                        imageVector = destination.unselectedIcon,
+                        contentDescription = null,
+                    )
+                },
+                selectedIcon = {
+                    Icon(
+                        imageVector = destination.selectedIcon,
+                        contentDescription = null,
+                    )
+                },
+            )
+        }
+    }
+}
+
+private fun NavDestination?.isTopLevelDestinationInHierarchy(destination: TopLevelDestination) =
+    this
+        ?.hierarchy
+        ?.any {
+            it.route?.contains(destination.name, true) ?: false
+        } ?: false
