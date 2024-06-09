de4dbf8dc6496b7feb06e84de5b163d7445447a8
citytexi <heekyne66@gmail.com>
2024-06-09 15:56:43 +0900

    add home gallery top bar, custom shadow

---
+++ feature/home/src/main/kotlin/com/captures2024/soongan/feature/home/ui/HomeGalleryScreen.kt
@@ -0,0 +1,11 @@
+package com.captures2024.soongan.feature.home.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+internal fun HomeGalleryScreen(
+    modifier: Modifier = Modifier,
+) {
+
+}
