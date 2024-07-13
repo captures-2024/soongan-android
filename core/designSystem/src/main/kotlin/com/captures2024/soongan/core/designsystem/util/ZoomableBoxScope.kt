fb3ccd18b5e2171c53b930586ccbd42109997bad
citytexi <heekyne66@gmail.com>
2024-07-13 14:01:56 +0900

    [feat] Zoom 가능한 Component 추가

---
+++ core/designSystem/src/main/kotlin/com/captures2024/soongan/core/designsystem/util/ZoomableBoxScope.kt
@@ -0,0 +1,7 @@
+package com.captures2024.soongan.core.designsystem.util
+
+interface ZoomableBoxScope {
+    val scale: Float
+    val offsetX: Float
+    val offsetY: Float
+}
---
+++ core/designSystem/src/main/kotlin/com/captures2024/soongan/core/designsystem/util/ZoomableBoxScopeImpl.kt
@@ -0,0 +1,8 @@
+package com.captures2024.soongan.core.designsystem.util
+
+data class ZoomableBoxScopeImpl(
+    override val scale: Float,
+    override val offsetX: Float,
+    override val offsetY: Float
+) : ZoomableBoxScope
+
