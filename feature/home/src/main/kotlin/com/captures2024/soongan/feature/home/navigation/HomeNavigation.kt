b1d40d7a3793b69b86355f0be399e994c0058999
citytexi <heekyne66@gmail.com>
2024-07-16 14:12:47 +0900

    [temp] modify home nav logic

--- feature/home/src/main/kotlin/com/captures2024/soongan/feature/home/navigation/HomeNavigation.kt
+++ feature/home/src/main/kotlin/com/captures2024/soongan/feature/home/navigation/HomeNavigation.kt
@@ -5,26 +5,17 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 import com.captures2024.soongan.feature.home.route.HomeRoute
-import com.captures2024.soongan.feature.home.route.PhotoDetailRoute
 
 const val HOME_NAVIGATION_ROUTE = "home_route"
-const val PHOTO_DETAIL_NAVIGATION_ROUTE = "photo_detail_route"
 
 fun NavController.navigateToHome(navOptions: NavOptions? = null) {
     this.navigate(HOME_NAVIGATION_ROUTE, navOptions)
 }
 
-fun NavController.navigateToPhotoDetail(navOptions: NavOptions? = null) {
-    this.navigate(PHOTO_DETAIL_NAVIGATION_ROUTE, navOptions)
-}
-
 fun NavGraphBuilder.home() {
     composable(route = HOME_NAVIGATION_ROUTE) {
         HomeRoute(
-            navigateToFeed = navigateToFeed,
+            navigateToFeed = {},
         )
-    }
-    composable(route = PHOTO_DETAIL_NAVIGATION_ROUTE) {
-        PhotoDetailRoute()
     }
 }
--- feature/main/src/main/kotlin/com/captures2024/soongan/feature/main/navigation/MainRouteNavHost.kt
+++ feature/main/src/main/kotlin/com/captures2024/soongan/feature/main/navigation/MainRouteNavHost.kt
@@ -40,9 +40,7 @@ internal fun MainRouteNavHost(
         welcome(
             navigateToHome = navController::navigateToHome,
         )
-        home(
-            navigateToFeed = navController::navigateToFeed,
-        )
+        home()
         feed()
         awards()
         profile()
