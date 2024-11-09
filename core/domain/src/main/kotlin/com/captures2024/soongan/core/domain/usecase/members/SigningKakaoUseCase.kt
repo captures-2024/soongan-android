ceb24d2fe4c6567f7826bc10c0406c381ef432c3
citytexi <heekyne66@gmail.com>
2024-11-09 14:04:11 +0900

    [SG-16] 카카오 로그인 success logic 적용

--- core/domain/src/main/kotlin/com/captures2024/soongan/core/domain/usecase/members/SigningKakaoUseCase.kt
+++ core/domain/src/main/kotlin/com/captures2024/soongan/core/domain/usecase/members/SigningKakaoUseCase.kt
@@ -11,11 +11,14 @@ constructor(
     private val repository: MembersRepository,
 ) {
 
-    suspend operator fun invoke(token: String): Result<Boolean> = runSuspendCatching {
+    suspend operator fun invoke(
+        token: String,
+        fcmToken: String
+    ): Result<Boolean> = runSuspendCatching {
         repository.signingSocialPlatform(
             type = SocialSignType.KAKAO,
             token = token,
-            fcmToken = "",
+            fcmToken = fcmToken,
         ).result
     }
 }
--- feature/signIn/src/main/kotlin/com/captures2024/soongan/feature/signIn/SignInViewModel.kt
+++ feature/signIn/src/main/kotlin/com/captures2024/soongan/feature/signIn/SignInViewModel.kt
@@ -54,7 +54,7 @@ constructor(
 
             is SignInIntent.CompleteSignGoogle -> googleSignIn(token = intent.token)
 
-            is SignInIntent.CompleteSignKakao -> TODO()
+            is SignInIntent.CompleteSignKakao -> kakaoSignIn(token = intent.accessToken)
 
             is SignInIntent.FetchFCMToken -> fetchFcmToken(token = intent.token)
 
@@ -117,6 +117,26 @@ constructor(
             copy(isLoading = true)
         }
         postSideEffect(SignInSideEffect.KakaoSignIn)
+    }
+
+    private fun kakaoSignIn(token: String) = launch {
+        val result = signingKakaoUseCase(
+            token = token,
+            fcmToken = currentState.fcmToken
+        ).getOrNull()
+
+        if (result == null) {
+            analyticsHelper.d(message = "result is null")
+            intent(SignInIntent.FailedSignKakao)
+            return@launch
+        }
+
+        analyticsHelper.d(message = "result = $result")
+
+        when (result) {
+            true -> isAllowCheck()
+            false -> failedSignIn()
+        }
     }
 
     private fun fetchFcmToken(token: String) {
