0b8b9e922c31a763778d70c98bb9e5adce455508
jbrunoo <6pizz@naver.com>
2024-12-01 19:18:39 +0900

    [SG-52] feat: 프로필 하단 시트 구현

---
+++ feature/profile/src/main/kotlin/com/captures2024/soongan/feature/profile/navigation/ProfileLevelDestination.kt
@@ -0,0 +1,46 @@
+package com.captures2024.soongan.feature.profile.navigation
+
+import androidx.annotation.StringRes
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import com.captures2024.soongan.core.designsystem.icon.MyIconPack
+import com.captures2024.soongan.core.designsystem.icon.myiconpack.IconArrowRightFromBracket
+import com.captures2024.soongan.core.designsystem.icon.myiconpack.IconFillPersonRunning
+import com.captures2024.soongan.core.designsystem.icon.myiconpack.IconNonFillCircleQuestion
+import com.captures2024.soongan.core.designsystem.icon.myiconpack.IconNonFillFile
+import com.captures2024.soongan.core.designsystem.icon.myiconpack.IconNonFillGear
+import com.captures2024.soongan.core.designsystem.icon.myiconpack.IconNonFillUser
+import com.captures2024.soongan.core.designsystem.theme.PrimaryA
+import com.captures2024.soongan.feature.profile.R
+
+internal enum class ProfileLevelDestination(
+    @StringRes val titleRes: Int,
+    val icon: ImageVector,
+    val color: Color = PrimaryA,
+) {
+    EDIT(
+        titleRes = R.string.profile_menu_bottom_sheet_edit_title,
+        icon = MyIconPack.IconNonFillUser,
+    ),
+    NOTIFICATION(
+        titleRes = R.string.profile_menu_bottom_sheet_notification_title,
+        icon = MyIconPack.IconNonFillGear,
+    ),
+    TERMS_AND_POLICY(
+        titleRes = R.string.profile_menu_bottom_sheet_temp_and_policy_title,
+        icon = MyIconPack.IconNonFillFile,
+    ),
+    FAQ(
+        titleRes = R.string.profile_menu_bottom_sheet_faq_title,
+        icon = MyIconPack.IconNonFillCircleQuestion,
+    ),
+    WITHDRAW(
+        titleRes = R.string.profile_menu_bottom_sheet_withdraw_title,
+        icon = MyIconPack.IconFillPersonRunning,
+    ),
+    SIGN_OUT(
+        titleRes = R.string.profile_menu_bottom_sheet_sign_out_title,
+        icon = MyIconPack.IconArrowRightFromBracket,
+        color = Color(0xFFFC0000)
+    )
+}
---
+++ feature/profile/src/main/kotlin/com/captures2024/soongan/feature/profile/ui/profile/ProfileMenuBottomSheet.kt
@@ -0,0 +1,105 @@
+package com.captures2024.soongan.feature.profile.ui.profile
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.SheetState
+import androidx.compose.material3.SheetValue
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.captures2024.soongan.core.designsystem.component.HeightSpacer
+import com.captures2024.soongan.core.designsystem.component.NonScaleText
+import com.captures2024.soongan.core.designsystem.theme.NanumSquareNeoFontFamily
+import com.captures2024.soongan.core.designsystem.theme.PrimaryA
+import com.captures2024.soongan.core.designsystem.util.DevicePreviews
+import com.captures2024.soongan.feature.profile.navigation.ProfileLevelDestination
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+internal fun ProfileMenuBottomSheet(
+    modifier: Modifier = Modifier,
+    sheetState: SheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true),
+    closeSheet: () -> Unit = {},
+    onClickMenuItem: (ProfileLevelDestination) -> Unit = {},
+) {
+    ModalBottomSheet(
+        onDismissRequest = closeSheet,
+        modifier = modifier.fillMaxWidth(),
+        sheetState = sheetState,
+        containerColor = Color.White
+    ) {
+        Column(
+            modifier = Modifier.padding(horizontal = 24.dp)
+        ) {
+            ProfileLevelDestination.entries.forEachIndexed { idx, item ->
+                ProfileMenuRow(
+                    item = item,
+                    onClick = onClickMenuItem
+                )
+                HeightSpacer(16.dp)
+                if (idx != ProfileLevelDestination.entries.lastIndex) {
+                    HorizontalDivider(color = PrimaryA.copy(alpha = 0.3f))
+                    HeightSpacer(16.dp)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun ProfileMenuRow(
+    modifier: Modifier = Modifier,
+    item: ProfileLevelDestination,
+    onClick: (ProfileLevelDestination) -> Unit = {},
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
+            .clickable { onClick(item) },
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        NonScaleText(
+            text = stringResource(item.titleRes),
+            fontSize = 16.sp,
+            color = item.color,
+            fontWeight = FontWeight.Bold,
+            fontFamily = NanumSquareNeoFontFamily,
+            letterSpacing = 0.sp,
+            lineHeight = 24.sp
+        )
+        Icon(
+            imageVector = item.icon,
+            contentDescription = stringResource(item.titleRes),
+            tint = item.color
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@DevicePreviews
+@Composable
+private fun ProfileMenuBottomSheetPreview() {
+    val sheetState = SheetState(
+        skipPartiallyExpanded = true,
+        initialValue = SheetValue.Expanded,
+        density = LocalDensity.current,
+        skipHiddenState = false
+    )
+
+    ProfileMenuBottomSheet(sheetState = sheetState)
+}
