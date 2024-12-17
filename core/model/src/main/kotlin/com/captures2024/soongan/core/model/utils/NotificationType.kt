c1705e964ae067173e783f86cf0c2508b6a311aa
jbrunoo <6pizz@naver.com>
2024-12-17 23:57:18 +0900

    [SG-59] fix: notificationType 변경

--- core/model/src/main/kotlin/com/captures2024/soongan/core/model/utils/NotificationType.kt
+++ core/model/src/main/kotlin/com/captures2024/soongan/core/model/utils/NotificationType.kt
@@ -1,11 +1,12 @@
 package com.captures2024.soongan.core.model.utils
 
 enum class NotificationType(
-    val title: String = "",
-    val deepLink: String? = null,
+    val deepLink: String? = null, // not decide yet. mail, appLink, google form etc..
 ) {
-    CONTEST("대회 알림"),
-    REPORT_EXPLANATION("활동 알림", deepLink = ""), // not decide yet. mail, appLink, google form etc..
-    ANNOUNCEMENT("공지 알림"),
+    CONTEST,
+    ACTION,
+    ANNOUNCEMENT,
+    VINDICATION,
     UNDEFINED
 }
+
