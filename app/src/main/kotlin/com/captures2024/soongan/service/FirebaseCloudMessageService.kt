a1a84f7c38cbe40461845ca04d3b72b65220da2e
jbrunoo <6pizz@naver.com>
2024-12-19 18:25:52 +0900

    [SG-59] fix: server 알림 메시지 형식 동기화

--- app/src/main/kotlin/com/captures2024/soongan/service/FirebaseCloudMessageService.kt
+++ app/src/main/kotlin/com/captures2024/soongan/service/FirebaseCloudMessageService.kt
@@ -7,14 +7,10 @@ import android.content.Intent
 import android.media.RingtoneManager
 import android.os.Build
 import android.util.Log
-import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
 import com.captures2024.soongan.R
 import com.captures2024.soongan.SoonGanActivity
 import com.captures2024.soongan.core.common.extension.checkGrantedPermission
-import com.captures2024.soongan.core.model.utils.NotificationType
-import com.captures2024.soongan.utils.getNotificationType
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import java.util.UUID
@@ -23,58 +19,61 @@ class FirebaseCloudMessageService : FirebaseMessagingService() {
 
     override fun onNewToken(token: String) {
         Log.d(TAG, "generate fcm token : $token")
-        // sendToServer - signIn 후 진행
     }
 
-    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onMessageReceived(message: RemoteMessage) {
-        Log.d(TAG, "generate fcm Message : $message")
+        Log.d(TAG, "received fcm Message : $message")
 
         if (!checkGrantedPermission(android.Manifest.permission.POST_NOTIFICATIONS)) return
-        if (message.data.isEmpty()) return
 
-        sendNotification(message.data)
+        message.notification?.let { sgNotification ->
+            val pendingIntent = createPendingIntent(message.data)
+            sendNotification(sgNotification, pendingIntent)
+        }
     }
 
-    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
-    private fun sendNotification(messageData: Map<String, String>) {
-        val type = getNotificationType(messageData["type"])
-        if (type == NotificationType.UNDEFINED) return
+    private fun createPendingIntent(messageData: Map<String, String>): PendingIntent {
+        // server 타입명 동기화 필요
+//        val type = getNotificationType(messageData["notificationType"])
 
         val intent = Intent(this, SoonGanActivity::class.java).apply {
             addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-            putExtra("notification_type", type.name)
-//            putExtra("hasNotification", "$count")
+//            putExtra("link", messageData["link"])
+//            putExtra("type", type)
+//            putExtra("postId", messageData["postId"])
+//            putExtra("timeStamp", messageData["timestamp"])
         }
-        val pendingIntent = PendingIntent.getActivity(
+
+        return PendingIntent.getActivity(
             this,
             UUID.randomUUID().hashCode(),
             intent,
             PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE
         )
+    }
+
+    private fun sendNotification(
+        sgNotification: RemoteMessage.Notification,
+        pendingIntent: PendingIntent,
+    ) {
+        val smallIcon = R.drawable.ic_noti_soongan
         val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
 
-        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_noti_soongan)
-            .setContentTitle(type.title)
-            .setContentText(messageData["title"])
-            .setStyle(
-                NotificationCompat.BigTextStyle()
-                    .bigText(messageData["body"])
-            )
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(smallIcon)
+            .setContentTitle(sgNotification.title)
+            .setContentText(sgNotification.body)
+//            .setStyle(NotificationCompat.BigTextStyle().bigText(sgNotification.body))
             .setAutoCancel(true)
             .setSound(defaultSoundUri)
             .setGroup(GROUP_KEY)
             .setContentIntent(pendingIntent)
+            .build()
 
         val summaryNotification = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setContentTitle("soongan")
-            .setContentText("새로운 알림이 있습니다")
-            .setSmallIcon(R.drawable.ic_noti_soongan)
-            .setStyle(
-                NotificationCompat.InboxStyle()
-                    .setSummaryText("순간에서 확인하세요")
-            )
+            .setSmallIcon(smallIcon)
+            .setContentTitle(GROUP_TITLE)
+            .setStyle(NotificationCompat.InboxStyle().setSummaryText(GROUP_TEXT))
             .setAutoCancel(true)
             .setOnlyAlertOnce(true)
             .setGroup(GROUP_KEY)
@@ -86,26 +85,29 @@ class FirebaseCloudMessageService : FirebaseMessagingService() {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                 val channel = NotificationChannel(
                     CHANNEL_ID,
-                    "notification for ${type.name}",
+                    CHANNEL_NAME,
                     NotificationManager.IMPORTANCE_HIGH
-                ).apply { description = "${type.name} notification channel" }
+                ).apply { description = CHANNEL_DESCRIPTION }
 
                 createNotificationChannel(channel)
             }
-        }
 
-        NotificationManagerCompat.from(this).apply {
-            if (!checkGrantedPermission(android.Manifest.permission.POST_NOTIFICATIONS)) return
-
-            notify(SUMMARY_NOTIFY_ID, summaryNotification)
-            notify((System.currentTimeMillis() / 7).toInt(), builder.build())
+            notify((System.currentTimeMillis() / 7).toInt(), notification)
+            notify(SUMMARY_ID, summaryNotification)
         }
     }
 
     companion object {
         private const val TAG = "FCMService"
-        private const val GROUP_KEY = "com.captures2024.soongan.NOTIFICATION.KEY"
+
+        private const val GROUP_KEY = "com.captures2024.soongan.NOTIFICATION_KEY"
+        private const val GROUP_TITLE = "soongan"
+        private const val GROUP_TEXT = "순간에서 확인하세요"
+
         private const val CHANNEL_ID = "soongan_channel_id"
-        private const val SUMMARY_NOTIFY_ID = 0
+        private const val CHANNEL_NAME = "soongan_Notification"
+        private const val CHANNEL_DESCRIPTION = "Channel for soongan Notification"
+
+        private const val SUMMARY_ID = 0
     }
 }
