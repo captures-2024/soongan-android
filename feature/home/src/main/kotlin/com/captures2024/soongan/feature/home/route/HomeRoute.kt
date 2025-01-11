a8c43d2f5ebc55f88077bb6f412ed1cab7a78150
citytexi <heekyne66@gmail.com>
2025-01-11 14:58:22 +0900

    [SG-82] feat: resume 상태에 돌입할 때마다 재조회

--- feature/home/src/main/kotlin/com/captures2024/soongan/feature/home/route/HomeRoute.kt
+++ feature/home/src/main/kotlin/com/captures2024/soongan/feature/home/route/HomeRoute.kt
@@ -2,6 +2,7 @@ package com.captures2024.soongan.feature.home.route
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
@@ -9,6 +10,9 @@ import androidx.compose.ui.draw.paint
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.captures2024.soongan.core.design.R
 import com.captures2024.soongan.core.designsystem.util.sgBottomBarPadding
@@ -48,6 +52,24 @@ internal fun HomeRoute(
 
                 is HomeSideEffect.NavigateToHomeGallery -> navigateToGallery()
             }
+        }
+    }
+
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    DisposableEffect(lifecycleOwner) {
+        val observer = LifecycleEventObserver { _, event ->
+            when (event) {
+                Lifecycle.Event.ON_RESUME -> homeViewModel.intent(HomeIntent.Init)
+
+                else -> Unit
+            }
+        }
+
+        lifecycleOwner.lifecycle.addObserver(observer)
+
+        onDispose {
+            lifecycleOwner.lifecycle.removeObserver(observer)
         }
     }
 
