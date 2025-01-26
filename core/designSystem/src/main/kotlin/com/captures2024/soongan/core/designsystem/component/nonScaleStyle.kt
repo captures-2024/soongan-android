f51df008bcba44a313afa723b29cefb80c8b38ad
jbrunoo <6pizz@naver.com>
2025-01-27 02:24:56 +0900

    [SG-94] feat: letterSpacing % 값 처리 로직 및 nonScaleTextStyle 추가
    ex) -0.05%
    letterSpacing = (-5).em

--- core/designSystem/src/main/kotlin/com/captures2024/soongan/core/designsystem/component/nonScaleStyle.kt
+++ core/designSystem/src/main/kotlin/com/captures2024/soongan/core/designsystem/component/nonScaleStyle.kt
@@ -3,12 +3,14 @@ package com.captures2024.soongan.core.designsystem.component
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.TextUnit
 import com.captures2024.soongan.core.designsystem.theme.PretendardFontFamily
 import com.captures2024.soongan.core.designsystem.util.nonScaleSp
+import com.captures2024.soongan.core.designsystem.util.normalizeLetterSpacing
 
 @Composable
 fun nonScaleSpanStyle(
@@ -21,6 +23,24 @@ fun nonScaleSpanStyle(
     fontSize = fontSize.nonScaleSp,
     fontFamily = fontFamily,
     fontWeight = fontWeight,
+    color = color,
+    textDecoration = textDecoration,
+)
+
+
+@Composable
+fun nonScaleTextStyle(
+    fontSize: TextUnit,
+    fontFamily: FontFamily = PretendardFontFamily,
+    fontWeight: FontWeight = FontWeight(500),
+    letterSPacing: TextUnit,
+    color: Color = Color(0xFF000000),
+    textDecoration: TextDecoration? = null,
+) = TextStyle(
+    fontSize = fontSize.nonScaleSp,
+    fontFamily = fontFamily,
+    fontWeight = fontWeight,
+    letterSpacing = normalizeLetterSpacing(fontSize, letterSPacing).nonScaleSp,
     color = color,
     textDecoration = textDecoration,
 )
