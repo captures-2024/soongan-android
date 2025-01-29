674ad5e326f5c845094215fc31a2a0c34e70d2c5
citytexi <heekyne66@gmail.com>
2025-01-29 13:22:27 +0900

    feat: add double button dialog

--- core/designSystem/src/main/kotlin/com/captures2024/soongan/core/designsystem/component/SGDefaultDialog.kt
+++ core/designSystem/src/main/kotlin/com/captures2024/soongan/core/designsystem/component/SGDefaultDialog.kt
@@ -2,9 +2,11 @@ package com.captures2024.soongan.core.designsystem.component
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -13,6 +15,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -93,6 +97,11 @@ fun SGSingleButtonDialog(
 
 @Composable
 fun SGDoubleButtonDialog(
+    content: String,
+    confirmContent: String,
+    onClickConfirm: () -> Unit,
+    cancelContent: String,
+    onClickCancel: () -> Unit,
     onDismissRequest: () -> Unit,
     properties: DialogProperties = DialogProperties(
         dismissOnBackPress = false,
@@ -110,10 +119,72 @@ fun SGDoubleButtonDialog(
                 .widthIn(min = 273.dp)
                 .heightIn(min = 240.dp)
                 .background(color = SGColor.white)
-                .padding(vertical = 12.dp),
+                .padding(vertical = 12.dp)
+                .padding(horizontal = 32.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween
         ) {
+            Image(
+                imageVector = MyIconPack.Logo,
+                contentDescription = "logo",
+                modifier = Modifier
+                    .width(33.1f.dp)
+                    .height(50.1f.dp)
+            )
 
+            NonScaleText(
+                text = content,
+                fontSize = 16.sp,
+                fontFamily = NanumSquareNeoFontFamily,
+                fontWeight = FontWeight.Bold,
+                color = SGColor.black,
+                lineHeight = 24.sp,
+                textAlign = TextAlign.Center,
+            )
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(21.dp)
+            ) {
+                Button(
+                    onClick = onClickCancel,
+                    modifier = Modifier.weight(1f)
+                        .height(48.dp)
+                        .border(
+                            width = 1.dp,
+                            color = SGColor.black,
+                            shape = RoundedCornerShape(8.dp),
+                        ),
+                    shape = RoundedCornerShape(8.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = SGColor.white,
+                        contentColor = SGColor.primaryA,
+                        disabledContainerColor = SGColor.primaryA.copy(alpha = 0.3f),
+                        disabledContentColor = SGColor.white
+                    ),
+                ) {
+                    NonScaleText(
+                        text = cancelContent,
+                        fontSize = 14.sp,
+                        fontFamily = NanumSquareNeoFontFamily,
+                        fontWeight = FontWeight.Bold,
+                        color = SGColor.black,
+                    )
+                }
+
+                SoonGanButton(
+                    onClick = onClickConfirm,
+                    modifier = Modifier.weight(1f)
+                ) {
+                    NonScaleText(
+                        text = confirmContent,
+                        fontSize = 14.sp,
+                        fontFamily = NanumSquareNeoFontFamily,
+                        fontWeight = FontWeight.Bold,
+                        color = SGColor.black,
+                    )
+                }
+            }
         }
     }
 }
@@ -127,6 +198,23 @@ private fun SGSingleButtonDialogPreview() {
                 content = "해당 기능은\n로그인이 필요한 기능입니다.",
                 confirmContent = "확인",
                 onClickConfirm = {},
+                onDismissRequest = {},
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun SGDoubleButtonDialogPreview() {
+    SoonGanTheme {
+        Box(Modifier.fillMaxSize()) {
+            SGDoubleButtonDialog(
+                content = "정말 댓글을\n삭제하시겠습니까?",
+                confirmContent = "삭제",
+                onClickConfirm = {},
+                cancelContent = "취소",
+                onClickCancel = {},
                 onDismissRequest = {},
             )
         }
