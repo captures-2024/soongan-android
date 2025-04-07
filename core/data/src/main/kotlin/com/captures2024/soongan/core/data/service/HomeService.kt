1aa486d9a7f728367e7af970b4cc4b7160d079a8
citytexi <heekyne66@gmail.com>
2025-04-07 13:03:00 +0900

    feat: modfiy network header

--- core/data/src/main/kotlin/com/captures2024/soongan/core/data/service/HomeService.kt
+++ core/data/src/main/kotlin/com/captures2024/soongan/core/data/service/HomeService.kt
@@ -1,5 +1,6 @@
 package com.captures2024.soongan.core.data.service
 
+import com.captures2024.soongan.core.model.AppConst
 import com.captures2024.soongan.core.model.network.response.BaseResponse
 import com.captures2024.soongan.core.model.network.response.home.GetHomeStatusResponse
 import retrofit2.Response
@@ -8,7 +9,10 @@ import retrofit2.http.Headers
 
 interface HomeService {
 
-    @Headers("Authorization: true")
+    @Headers(AppConst.Network.ACCESS_TOKEN_ALLOW)
     @GET("home")
-    suspend fun getHomeStatus(): Response<BaseResponse<GetHomeStatusResponse>>
+    suspend fun getHomeStatusWithToken(): Response<BaseResponse<GetHomeStatusResponse>>
+
+    @GET("home")
+    suspend fun getHomeStatusWithGuest(): Response<BaseResponse<GetHomeStatusResponse>>
 }
